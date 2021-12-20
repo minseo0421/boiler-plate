520387b6a1cbb5cb014c33e123ba50687d46733b
minseo0421 <58363224+minseo0421@users.noreply.github.com>
2021-12-20 11:23:02 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const express = require('express') // express 모듈 가져오기
+const app = express() // express 앱 만들기
+const port = 5000
+
+const mongoose = require('mongoose')
+mongoose.connect(config.mongoURI, {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err))
+app.get('/', (req, res) => res.send('Hello World!')) // /에 hello world 출력
+app.listen(port, () => console.log(`Example app listening on port ${port}!`)) //5000번에서 실행
 
